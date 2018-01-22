346823f38340c95b2c9d3b6d9fd4ef57704a8c3f
Eli Heuer <eliheuer@gmail.com>
2018-01-22 01:13:31 -0500

    adding requirements.txt file

--- ttf-to-ufo/ttf-to-ufo.py
+++ ttf-to-ufo/ttf-to-ufo.py
@@ -2,15 +2,12 @@
 import argparse
 import defcon
 import extractor
-#import os
-#import sys
 
 def create_arg_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", help = "input filename")
     parser.add_argument("--output", help = "output filename")
     args = parser.parse_args()
-    # console output
     print('inputfile: ', args.input)
     print('outputfile: ', args.output)
     return args
@@ -19,10 +16,8 @@ if __name__ == "__main__":
     args = create_arg_parser()
     ttf_path = args.input
     ufo_path = args.output
-    # console output
     print('ttf_path: ', ttf_path)
     print('ufo_path', ufo_path)
-
     # Make UFO
     ufo = defcon.Font()
     extractor.extractUFO(ttf_path, ufo)
