f9bd37228931e915b3002eb621324cf68552caea
Eli Heuer <eliheuer@gmail.com>
2018-01-22 02:09:53 -0500

    Change --input to -i, etc...

--- ttf-to-ufo/ttf-to-ufo.py
+++ ttf-to-ufo/ttf-to-ufo.py
@@ -1,21 +1,20 @@
-#! /usr/bin/env python
 import argparse
 import defcon
 import extractor
 
 def create_arg_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input", help = "input filename")
-    parser.add_argument("--output", help = "output filename")
+    parser.add_argument("-i", help = "input filename")
+    parser.add_argument("-o", help = "output filename")
     args = parser.parse_args()
-    print('inputfile: ', args.input)
-    print('outputfile: ', args.output)
+    print('inputfile: ', args.i)
+    print('outputfile: ', args.o)
     return args
 
 if __name__ == "__main__":
     args = create_arg_parser()
-    ttf_path = args.input
-    ufo_path = args.output
+    ttf_path = args.i
+    ufo_path = args.o
     print('ttf_path: ', ttf_path)
     print('ufo_path', ufo_path)
     # Make UFO
